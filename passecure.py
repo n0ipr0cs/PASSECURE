7c4c095141f7bcfd87b887d941e870ed7b58b7c6
n0ipr0cs <n0ipr0cs@users.noreply.github.com>
2016-04-12 15:10:32 +0100

    Update passecure.py

--- passecure.py
+++ passecure.py
@@ -3,12 +3,19 @@
 
 import string
 
+print	" ____       _      ____    ____    _____    ____   _   _   ____    _____ "
+print	"|  _ \     / \    / ___|  / ___|  | ____|  / ___| | | | | |  _ \  | ____|"
+print	"| |_) |   / _ \   \___ \  \___ \  |  _|   | |     | | | | | |_) | |  _|"
+print	"|  __/   / ___ \   ___) |  ___) | | |___  | |___  | |_| | |  _ <  | |___"
+print	"|_|     /_/   \_\ |____/  |____/  |_____|  \____|  \___/  |_| \_\ |_____|" 
+print "******************************by n0ipr0cs*********************************"
+
+
+print ""
+
 from random import*
 characters = string.ascii_letters + string.punctuation + string.digits
 password = "".join(choice(characters)for x in range(randint(8,16)))
-print "Tu password es:"
+print "Tu password es:" 
 print password
 print "Ya tienes un password fuerte"
-print "************************************"
-print "P A S S E C U R E  by n0ipr0cs"
-print "************************************"
